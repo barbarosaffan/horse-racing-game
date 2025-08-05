3ef9821df8720dabc3fc6f461403be1ed6a74c82
Barbaros YILDIRIM <barbarosayildirim@gmail.com>
2025-08-05 23:33:35 +0300

    feat: add horse item component

---
+++ src/modules/racing/components/HorseItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import ConditionBar from '@/core/components/ConditionBar.vue'
+
+import type { Horse } from '@/modules/racing/types'
+
+interface Props {
+  horse: Horse
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <li class="flex items-center p-2 bg-gray-50 dark:bg-gray-700/40 rounded-md justify-between">
+    <div class="flex items-center mr-4">
+      <div class="w-4 h-4 rounded-full mr-3" :style="{ backgroundColor: horse.color }"></div>
+      <span class="text-sm font-medium text-gray-700 dark:text-gray-300">{{ horse.name }}</span>
+    </div>
+    <ConditionBar :value="horse.condition" />
+  </li>
+</template>
